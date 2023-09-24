ef2d7641d00613740e116925ec3daaad2b8300dd
Nitin <nitinww53@gmail.com>
2023-09-24 01:35:38 -0400

    Created new tailwind theme Created new components like button and linkbutton

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { Link, useNavigate } from "react-router-dom";
+
+const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+  if (to === "-1") {
+    return (
+      <button
+        onClick={() => {
+          navigate(parseInt(to));
+        }}
+        className={className}
+      >
+        {children}
+      </button>
+    );
+  }
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
